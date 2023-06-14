3c4e709decbaa43db3141725014c91f33fab51ef
Wes Hinsley <w.hinsley@imperial.ac.uk>
2023-06-14 17:45:17 +0100

    Cross-platform quoting issue

--- tests/testthat/test-mpi.R
+++ tests/testthat/test-mpi.R
@@ -1,18 +1,23 @@
 test_mpi <- function(script, procs = 2) {
-  script <- paste0('"', script, '"')
+  cmdquote <- if (as.character(Sys.info()['sysname']) == "Windows") '"' else "'"
+
   suppressWarnings(
-    system2("mpiexec", sprintf(" -n %d Rscript -e %s", procs, script),
+    system2("mpiexec", sprintf(" -n %d Rscript -e %s%s%s", procs,
+                               cmdquote, script, cmdquote),
             stdout = TRUE, stderr = TRUE))
 
 }
 
 test_script <- function(n_particles, n_streams, out_file,
                         gamma, beta, freq, Si, Ii, Ri, time) {
+  file_quote <- if (as.character(Sys.info()['sysname']) == "Windows") "'" else '"'
+
   gsub("\n", " ", sprintf(
   "dustmpi:::simulate_model_to_file(list(gamma = %s, beta = %s, freq = %s),
       c(%s, %s, %s, 0, 0), %s, %s,
-      dust::dust_rng_pointer$new(42, n_streams = %s), '%s', TRUE)",
-      gamma, beta, freq, Si, Ii, Ri, time, n_streams, n_particles, out_file))
+      dust::dust_rng_pointer$new(42, n_streams = %s), %s%s%s, TRUE)",
+      gamma, beta, freq, Si, Ii, Ri, time, n_streams, n_particles,
+      file_quote, out_file, file_quote))
 }
 
 test_that("MPI Sim gives same result as single-processor", {
@@ -23,7 +28,8 @@ test_that("MPI Sim gives same result as single-processor", {
   res1 <- simulate_model(pars, y, end_time, 10, rng1, FALSE)
 
   tmp <- normalizePath(tempfile(), winslash = "/", mustWork = FALSE)
-  script <- test_script(10, 10, tmp, pars$gamma, pars$beta, pars$freq, y[1], y[2], y[3], end_time)
+  script <- test_script(10, 10, tmp, pars$gamma, pars$beta, pars$freq,
+                        y[1], y[2], y[3], end_time)
   test_mpi(script, 10)
   res2 <- readRDS(tmp)
 
