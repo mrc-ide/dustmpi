8cb6844fd59647054a9d2e13e03d74f4b9f188c4
Wes Hinsley <w.hinsley@imperial.ac.uk>
2023-06-14 18:02:05 +0100

    Make mpi_wrapper even hackier

--- R/mpi_wrapper.R
+++ R/mpi_wrapper.R
@@ -1,6 +1,6 @@
 simulate_model_to_file <- function(pars, y, end_time, n_particles,
                                    rng, output_file, use_mpi) {
-  res <- simulate_model(pars, y, end_time, n_particles, rng, use_mpi)
+  res <- dustmpi:::simulate_model(pars, y, end_time, n_particles, rng, use_mpi)
 
   if ((!is.null(output_file)) && (length(res) > 0)) {
     saveRDS(res, output_file)
